6ec116f03d9953d292adb32a04416cd9ec129043
Li Yi <yili4u@gmail.com>
2024-06-17 15:49:16 -0700

    Added createPath() function to create paths of the maze.

--- src/MazeGrid.js
+++ src/MazeGrid.js
@@ -16,19 +16,48 @@ export default function MazeGrid(){
     function generateMaze(height, width){
         let maze = [];
 
+        // Initialize the maze of all walls
         for (let i = 0; i < height; i++){
             let row = [];
             for (let j = 0; j < width; j++){
-                let cell = 'wall';
-                if (Math.random() < 0.5){
-                    cell = 'path';
-                }
-                row.push(cell)
+                row.push('wall')
             }
             maze.push(row)
         }
+
+        // Create paths
+        const left = [0, -1], right = [0, 1], up = [-1, 0], down = [1, 0]; 
+        const dirs = [left, right, up, down];
+
+        function isCellValid(r, c){
+            return r >= 0 && r < height && c >= 0 && c < width &&
+                   maze[r][c] === 'wall';
+        }
+
+       function createPath(r, c){
+            maze[r][c] = 'path';
+
+            // Randomly pick an order
+            const directions = dirs.sort(() => Math.random() - 0.5);
+
+            // Try each direction
+            for (let [dr, dc] of directions){
+                // Check next cell of the same direction
+                const nr = r + dr * 2;
+                const nc = c + dc * 2;
+                if (isCellValid(nr, nc)){
+                    maze[r + dr][c + dc] = 'path';
+                    createPath(nr, nc);
+                }
+            }
+        }
+
+        createPath(1,1);
+
+        // Set maze start and end points
         maze[1][0] = 'start';
         maze[height-2][width-1] = 'end';
+
         setMaze(maze);
     }
 
